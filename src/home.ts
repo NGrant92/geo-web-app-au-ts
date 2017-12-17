0d8dc48f7dcf5f8f17d01aea5528a7d5828cbdde
NGrant <niall.grant1992@gmail.com>
2017-12-17 17:07:57 +0000

    basic homepage setup for when user logs in

---
+++ src/home.ts
@@ -0,0 +1,25 @@
+import { RouterConfiguration, Router } from 'aurelia-router';
+
+export class Home {
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.map([
+      {
+        route: ['', 'home'],
+        name: 'geo',
+        moduleId: 'components/add-cache',
+        nav: true,
+        title: 'Home',
+      },
+      {
+        route: 'logout',
+        name: 'logout',
+        moduleId: 'components/logout',
+        nav: true,
+        title: 'Logout',
+      },
+    ]);
+    this.router = router;
+  }
+}
