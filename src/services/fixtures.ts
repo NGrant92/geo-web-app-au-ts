0e2e357bba2f2ec9f85a37994b8cc39ce0ebc28d
NGrant <niall.grant1992@gmail.com>
2017-12-17 13:43:10 +0000

    Added user object array to fixtures.ts. Changed Javascript target in tsconfig.json to 'es6'

--- src/services/fixtures.ts
+++ src/services/fixtures.ts
@@ -3,6 +3,40 @@ import {Cache, MessagePost, User} from "./models";
 export default class Fixtures {
   baseUrl = 'http://localhost:4000';
 
+  users: Map<string, User> = new Map()
+    .set('homer@simpson.com', {
+      firstName: 'Homer',
+      lastName: 'Simpson',
+      email: 'homer@simpson.com',
+      password: 'secret',
+      img: "http://res.cloudinary.com/ngrant/image/upload/v1506255274/LhFydmk_ueraaq.png",
+      admin: false,
+    })
+    .set('marge@simpson.com', {
+      firstName: 'Marge',
+      lastName: 'Simpson',
+      email: 'marge@simpson.com',
+      password: 'secret',
+      img: "http://res.cloudinary.com/ngrant/image/upload/v1509621811/marge_efpuft.jpg",
+      admin: false,
+    })
+    .set('bart@simpson.com', {
+      firstName: 'Bart',
+      lastName: 'Simpson',
+      email: 'bart@simpson.com',
+      password: 'secret',
+      img: "http://res.cloudinary.com/ngrant/image/upload/v1503767824/gteyxykqycd8zu2jwtiy.png",
+      admin: false,
+    })
+    .set('admin@simpson.com', {
+      firstName: 'Ned',
+      lastName: 'Flanders',
+      email: 'admin@simpson.com',
+      password: 'secret',
+      img: "https://res.cloudinary.com/ngrant/image/upload/v1509798959/ned.jpg",
+      admin: true,
+    });
+
   caches: Array<Cache> = [
     {
       name: "Skatepark Cache",
