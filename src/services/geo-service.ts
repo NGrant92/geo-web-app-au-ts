2c18c0044f3e961341d70b944e9549bdceecb015
NGrant <niall.grant1992@gmail.com>
2017-12-19 11:18:51 +0000

    geo-service.ts/addCache() and addMessagePost() now use API calls to interact with mlab database

--- src/services/geo-service.ts
+++ src/services/geo-service.ts
@@ -1,20 +1,20 @@
-import { inject } from 'aurelia-framework';
-import Fixtures from './fixtures';
-import AsyncHttpClient from './async-http-client';
+import { inject } from "aurelia-framework";
+import Fixtures from "./fixtures";
+import AsyncHttpClient from "./async-http-client";
 
-import { EventAggregator } from 'aurelia-event-aggregator';
-import { LoginStatus } from './messages';
+import { EventAggregator } from "aurelia-event-aggregator";
+import { LoginStatus } from "./messages";
 import { Cache, MessagePost, User } from "./models";
 
 @inject(Fixtures, EventAggregator, AsyncHttpClient)
-export class GeoService{
+export class GeoService {
   ea: EventAggregator;
   ac: AsyncHttpClient;
-  users: Map<string, User>;
+  users: Map<string, User> = new Map();
   caches: Array<Cache> = [];
   messagePosts: Array<MessagePost> = [];
 
-  constructor(data: Fixtures, ea: EventAggregator, ac: AsyncHttpClient){
+  constructor(data: Fixtures, ea: EventAggregator, ac: AsyncHttpClient) {
     this.ea = ea;
     this.ac = ac;
     this.getUsers();
@@ -23,7 +23,7 @@ export class GeoService{
   }
 
   getUsers() {
-    this.ac.get('/api/users').then(res => {
+    this.ac.get("/api/users").then(res => {
       const users = res.content as Array<User>;
       users.forEach(user => {
         this.users.set(user.email.toString(), user);
@@ -31,77 +31,94 @@ export class GeoService{
     });
   }
 
-  getCaches(){
-    this.ac.get('/api/caches').then(res => {
+  getCaches() {
+    this.ac.get("/api/caches").then(res => {
       this.caches = res.content;
     });
   }
 
-  getMessagePosts(){
-    this.ac.get('api/messages').then(res => {
+  getMessagePosts() {
+    this.ac.get("api/messages").then(res => {
       this.messagePosts = res.content;
-    })
+    });
   }
 
-  addMessage(newMessage: string, newOwner: User){
+  addMessagePost(newMessage: string, newOwner: User) {
     const newMessagePost = {
       message: newMessage,
-      owner: newOwner,
+      owner: newOwner
     };
+
+    this.ac
+      .post("/api/messages", newMessagePost)
+      .then(res => {
+        this.caches.push(res.content);
+        console.log("Message successfully posted");
+      })
+      .catch(err => {
+        console.log(err);
+      });
 
     this.messagePosts.push(newMessagePost);
     console.log(`${newMessage} added successfully`);
   }
 
-  addCache(newName: string, newLocation: string, newLat: Number, newLong: Number, newDesc: string){
+  addCache(newName: string, newLocation: string, newLat: Number, newLong: Number, newDesc: string) {
     const newCache = {
       name: newName,
       location: newLocation,
       latitude: newLat,
       longitude: newLong,
       description: newDesc,
-      owner: this.users[0],
+      owner: this.users[0]
     };
 
-    this.caches.push(newCache);
-    console.log(`${newName} added successfully`);
+    this.ac
+      .post("/api/caches", newCache)
+      .then(res => {
+        this.caches.push(res.content);
+        console.log(`${newName} added successfully`);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
-  addUser(newFirstName, newLastName, newEmail, newPassword){
+  addUser(newFirstName, newLastName, newEmail, newPassword) {
     const newUser = {
       firstName: newFirstName,
       lastName: newLastName,
       email: newEmail,
       password: newPassword,
       img: "http://res.cloudinary.com/ngrant/image/upload/v1513088924/white-pin-green-back_iy4fax.png",
-      admin: false,
+      admin: false
     };
 
     this.users.set(newEmail, newUser);
     console.log(`${newFirstName} ${newLastName} added successfully`);
   }
 
-  login(email: string, password: string){
+  login(email: string, password: string) {
     const loginStatus = new LoginStatus(false);
     const user = this.users.get(email);
 
-    if (user){
-      if (user.password === password){
+    if (user) {
+      if (user.password === password) {
         loginStatus.status = true;
-        loginStatus.loginMessage = 'logged in';
+        loginStatus.loginMessage = "logged in";
       }
       else {
-        loginStatus.loginMessage = 'Incorrect Password';
+        loginStatus.loginMessage = "Incorrect Password";
       }
     }
     else {
-      loginStatus.loginMessage = 'Unregistered email';
+      loginStatus.loginMessage = "Unregistered email";
     }
     this.ea.publish(loginStatus);
     console.log(`User logged in`);
   }
 
-  logout(){
+  logout() {
     this.ea.publish(new LoginStatus(false));
     console.log(`User logged out`);
   }
