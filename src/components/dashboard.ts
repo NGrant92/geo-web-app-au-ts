5436c6dd090457852dc8fa5fa744947843c98bdd
NGrant <niall.grant1992@gmail.com>
2017-12-21 22:40:53 +0000

    fixed layout of add cache feature

--- src/components/dashboard.ts
+++ src/components/dashboard.ts
@@ -1,5 +1,15 @@
 import * as $ from 'jquery'
+import { inject } from 'aurelia-framework';
+import { GeoService } from '../services/geo-service';
+
+@inject(GeoService)
 export class Dashboard {
+  geoService: GeoService;
+
+  constructor(gs: GeoService) {
+    this.geoService = gs;
+    this.geoService.getLoggedUser();
+  }
 
   attached(){
     $(document).ready(function(){
