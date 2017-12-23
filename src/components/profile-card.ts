14a4be9fd1d35b8616088b9704e6a85e25ef3e5b
NGrant <niall.grant1992@gmail.com>
2017-12-23 12:25:13 +0000

    user object in profile-card.ts now uses an event aggregator to auto update user details

--- src/components/profile-card.ts
+++ src/components/profile-card.ts
@@ -1,14 +1,22 @@
 import { inject } from 'aurelia-framework';
 import { GeoService } from "../services/geo-service";
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { CurrentUser } from "../services/messages";
 import { User } from '../services/models';
 
-@inject(GeoService)
+@inject(GeoService, EventAggregator)
 export class ProfileCard{
   geoService: GeoService;
+  ea: EventAggregator;
   user: User;
 
-  constructor(gs){
+  constructor(gs: GeoService, ea: EventAggregator){
     this.geoService = gs;
-    this.user = this.geoService.currUser;
+    //this.user = this.geoService.currUser;
+
+    ea.subscribe(CurrentUser, msg => {
+      this.user = msg.currUser;
+    })
+
   }
 }
