156ee71cb3bf4a2ec3e8a3aa7c4c38d8e41e7dfc
NGrant <niall.grant1992@gmail.com>
2018-01-03 18:41:51 +0200

    profile-card.ts/addFollowing() and removeFollowing() now update isUserFollowing boolean variable

--- src/components/profile-card.ts
+++ src/components/profile-card.ts
@@ -1,36 +1,34 @@
 import { inject } from 'aurelia-framework';
 import { GeoService } from "../services/geo-service";
 import { EventAggregator } from 'aurelia-event-aggregator';
-import {
-  CurrentUser, CurrUserFollowees, CurrUserFollowers, FoundUserFollowees,
-  GetUser
-} from "../services/messages";
-import {Following, User} from '../services/models';
+import { CurrUserFollowers, FoundUserFollowees, GetUser } from "../services/messages";
+import { User } from '../services/models';
 
 @inject(GeoService, EventAggregator)
 export class ProfileCard{
   geoService: GeoService;
+  //ea: EventAggregator;
   user = null;
   isLoggedUser: boolean = false;
-  isFollowing: boolean = false;
+  isUserFollowing: boolean = false;
   followerList: Array<User>;
   followeeList: Array<User>;
 
   constructor(gs: GeoService, ea: EventAggregator){
     this.geoService = gs;
+    //this.ea = ea;
 
     ea.subscribe(GetUser, msg => {
-      const foundUser = msg.foundUser as User;
-      this.user = foundUser;
+      this.user = msg.foundUser as User;
 
-      this.isLoggedUser = foundUser._id === this.geoService.currUser._id;
+      this.isLoggedUser = this.user._id === this.geoService.currUser._id;
 
       //looping through the logged in user's list of people they're following to see
       //if logged user is following the person they're viewing
-      this.isFollowing = false;
+      this.isUserFollowing = false;
       for(let followee of this.geoService.currUserFollowees){
-        if(followee._id === foundUser._id){
-          return this.isFollowing = true;
+        if(followee._id === this.user._id){
+          return this.isUserFollowing = true;
         }
       }
     });
@@ -42,13 +40,25 @@ export class ProfileCard{
     ea.subscribe(FoundUserFollowees, msg => {
       this.followeeList = msg.followeeList;
     });
+
+    // ea.subscribe(isFollowing, msg =>{
+    //   this.isUserFollowing = msg.isFollowing;
+    // });
+
+    // ea.subscribe(CurrUserFollowees, msg => {
+    //   this.currUserFolloweeList = msg.currUserFollowees;
+    //
+    //   this.checkFollowing();
+    // })
   }
 
   addFollowing(){
     this.geoService.addFollower(this.user._id);
+    this.isUserFollowing = true;
   }
 
   removeFollowing(){
     this.geoService.removeFollower(this.user._id);
+    this.isUserFollowing = false;
   }
 }
